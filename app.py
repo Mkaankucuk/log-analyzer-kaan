69815fe624082485416ca725bc57364f7c5584eb
MUHAMMED KAAN KUCUK <TCMUHKUCUK@turkcell.entp.tgc>
2026-03-27 14:59:24 +0300

    timestamp ve session eklendi

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey123"
 
-# Şimdilik sabit admin bilgisi
+# Sabit admin
 ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "1234"
 
-# Sistem logları
+# Loglar
 logs = []
-
-# Login metrikleri
 successful_logins = []
 failed_logins = []
 
@@ -25,24 +25,36 @@ def login():
     password = request.form.get("password")
 
     if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+        session["user"] = username
+
         successful_logins.append({
             "username": username,
-            "message": "Başarılı login"
+            "message": "Başarılı login",
+            "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         })
+
         return redirect(url_for("dashboard"))
+
     else:
         failed_logins.append({
             "username": username,
-            "message": "Hatalı login denemesi"
+            "message": "Hatalı login",
+            "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         })
+
         return render_template("index.html", error="Kullanıcı adı veya şifre hatalı.")
 
 
 @app.route("/dashboard")
 def dashboard():
+    if "user" not in session:
+        return redirect(url_for("home"))
+
+    user = session.get("user")
+
     total_logs = len(logs)
-    error_logs = len([log for log in logs if log["type"] == "ERROR"])
-    warning_logs = len([log for log in logs if log["type"] == "WARNING"])
+    error_logs = len([log for log in logs if log.get("type") == "ERROR"])
+    warning_logs = len([log for log in logs if log.get("type") == "WARNING"])
 
     failed_login_count = len(failed_logins)
     successful_login_count = len(successful_logins)
@@ -56,16 +68,20 @@ def dashboard():
 
     return render_template(
         "dashboard.html",
+        user=user,
         total_logs=total_logs,
         error_logs=error_logs,
         warning_logs=warning_logs,
         failed_login_count=failed_login_count,
-        failed_login_rate=round(failed_login_rate, 2)
+        failed_login_rate=round(failed_login_rate, 2),
+        failed_logins=failed_logins,
+        successful_logins=successful_logins
     )
 
 
 @app.route("/logout")
 def logout():
+    session.pop("user", None)
     return redirect(url_for("home"))
 
 
